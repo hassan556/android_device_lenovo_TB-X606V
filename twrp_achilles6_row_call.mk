bb58f85a664357790ad0bb7cea193c4438dda84e
hassan556 <hassanm19@gmail.com>
2024-03-31 17:53:04 +0530

    Fix errors

--- twrp_achilles6_row_call.mk
+++ twrp_achilles6_row_call.mk
@@ -19,15 +19,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from achilles6_row_call device
-$(call inherit-product, device/lenovo/achilles6_row_call/device.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := achilles6_row_call
